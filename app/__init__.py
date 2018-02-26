4819b474bc995813bae8220713140d869e4e1da7
Robert Ruiz <rsblooxpert@gmail.com>
2018-02-26 14:36:20 -0800

    Login and Signup form implemented, partially login
    The front end has the templates for the form. The signup form connects to the DB now. Requirements.txt updated. Updated config.py.

--- app/__init__.py
+++ app/__init__.py
@@ -2,16 +2,16 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
 
+login = LoginManager(app)
+
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
-# This line will allow the app to auto update: Consider removing it for production use.
-app.config['DEBUG'] = True
 
 
 from app import routes,models,forms
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,10 @@
 from flask import render_template,send_file,request,redirect,url_for,flash,jsonify
 from app import app
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, logout_user
 from app.models import User
-from flask_login import logout_user
 from app import db
-from app.forms import RegistrationForm
+from app.forms import RegistrationForm, LoginForm
+
 # TODO: Implement main stock page:
 @app.route('/')
 @app.route('/index')
@@ -17,6 +17,7 @@ def login():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = LoginForm()
+    signup = RegistrationForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
@@ -24,10 +25,16 @@ def login():
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
-# TODO: Implement Signup Page NOTE: Will not be get, just post.
-@app.route('/signup')
+    return render_template('login.html', title='Sign In', LoginForm=form,RegistrationForm=signup)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/signup', methods=['POST'])
 def signup():
+    form = RegistrationForm(request.form)
     if form.validate_on_submit():
         user = User(username=form.username.data)
         user.set_password(form.password.data)
@@ -35,7 +42,8 @@ def signup():
         db.session.commit()
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
-    return render_template('signup.html', title='Signup', form=form)
+    else:
+        return redirect(url_for('login'))
 
 # TODO: Implement Users Page
 @app.route('/users')
@@ -46,10 +54,7 @@ def userPage():
 @app.route('/resources')
 def resources():
     return render_template('resources.html')
-@app.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
+
 # TODO: Implement Dynamic Currency Page
 @app.route('/currency')
 def currency():
