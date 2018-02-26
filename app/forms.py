2f5a3c1d3cd4bae337935cb00b51baa90ce7219a
Robert Ruiz <rsblooxpert@gmail.com>
2018-02-26 14:36:20 -0800

    Signup form implemented, partially login
    The front end has the templates for the forms. The signup form connects to the DB now. Requirements.txt updated. Updated config.py.

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
+from app.models import User
+
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -11,6 +13,7 @@ class LoginForm(FlaskForm):
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
+    # confirmPassword = PasswordField
     submit = SubmitField('Register')
 
     def validate_username(self, username):
